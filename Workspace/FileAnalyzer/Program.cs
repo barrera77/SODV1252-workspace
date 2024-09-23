eff03b7f9c00093b127ba2e3ccdeea77faca536f
barrera77 <30675869+barrera77@users.noreply.github.com>
2024-09-22 22:57:09 -0600

    Started Final Project (Created Sale Class and Required Methods Skeletons)

--- Workspace/FileAnalyzer/Program.cs
+++ Workspace/FileAnalyzer/Program.cs
@@ -6,5 +6,75 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        #region Main Tasks
+        static List<Sale> ReadSalesData(string file)
+        {
+            List<Sale> sales = new List<Sale>();    
+
+
+
+
+            return sales;
+
+        }
+
+        static void DisplayTotalSalesByProduct(List<Sale> sales, string title)
+        {
+
+        }
+
+        static void DisplayTotalSalesByMonth(List<Sale> sales, string title)
+        {
+
+        }
+
+        static void WriteSalesData(List<Sale> sales, string file)
+        {
+
+        }
+
+        #endregion
+
+        #region Helper Methods
+        //Validate file name
+        static bool IsValidFileName(string fileName)
+        {
+            return !String.IsNullOrWhiteSpace(fileName);
+        }
+
+        /// <summary>
+        /// Validates user input
+        /// </summary>
+        /// <param name="prompt"></param>
+        /// <param name="element"></param>
+        /// <returns>validated input</returns>
+        static string ValidateInput(string prompt, string element)
+        {
+            bool isValidInput = false;
+            string input = "";
+
+            Console.Write(prompt);
+
+            while (!isValidInput)
+            {
+
+                input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.Write($"Invalid input. Please enter a valid {element}:");
+                }
+                else
+                {
+                    isValidInput = true;
+                }
+            }
+            return input;
+        }
+
+       
+
+        #endregion
     }
 }
---
+++ Workspace/FileAnalyzer/Sale.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAnalyzer
+{
+    internal class Sale
+    {
+        private string _productName;
+        private DateOnly _dateOfSale;
+        private decimal _salesAmount;
+
+        //Default Constructor
+
+
+        //ProductName
+        public string ProductName
+        {
+            get { return _productName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid input, please provide a name for the product");
+                }
+                else
+                {
+                    _productName = value.Trim();
+                }
+            }
+        }
+
+        //Date of Sale
+        public DateOnly DateOfSale
+        { 
+            get { return _dateOfSale; } 
+            set
+            {
+                _dateOfSale = value;
+            }
+        }
+
+        public decimal SalesAmount
+        { 
+            get { return _salesAmount; }
+            set
+            {
+                if(_salesAmount < 0)
+                {
+                    throw new ArgumentException("Invalid input, please provide a valid amount");
+                }
+                _salesAmount = value;
+            }
+        }
+
+        //Greedy Constructor
+        public Sale(string productName, DateOnly dateOfSale, decimal salesAmount)
+        {
+            ProductName = productName;
+            DateOfSale = dateOfSale; ;
+            SalesAmount = salesAmount;
+        }
+
+
+
+    }
+}
